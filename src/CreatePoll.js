5089a724059cb0b166cd0159cb71a28d5cdb40a0
SergeChurkin-ca <sergechurkin@yahoo.com>
2020-08-15 13:04:49 -0400

    first structure and links

---
+++ src/CreatePoll.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+// import { Link } from "react-router-dom";
+
+class CreatePoll extends Component {
+  render() {
+    return (
+      <main>
+        <section>
+          <h1>Poll</h1>
+        </section>
+      </main>
+    );
+  }
+}
+export default CreatePoll;
