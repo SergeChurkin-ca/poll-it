6f622bb96ca606d34eb5f49d651c2d0c120cfe5b
Arman Aliani <armanaliani@gmail.com>
2020-08-15 16:19:50 -0400

    creatpoll input to firebase

--- src/App.js
+++ src/App.js
@@ -1,42 +1,13 @@
 import React, { Component } from "react";
-import firebase from "./firebase";
 import HomePage from "./HomePage";
 import CreatePoll from "./CreatePoll";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      testArray: [],
-    };
-  }
-
-  componentDidMount() {
-    const dbRef = firebase.database().ref();
-    dbRef.on("value", (snapshot) => {
-      const data = snapshot.val();
-      const newTestArray = [];
-      for (let key in data) {
-        newTestArray.push({
-          key: key,
-          data: data[key],
-        });
-      }
-      this.setState({
-        testArray: newTestArray,
-      });
-    });
-  }
-
   render() {
     return (
       <Router>
         <div>
-          {this.state.testArray.map((item) => {
-            return <p key={item.key}> {item.data} </p>;
-          })}
-          <input type="text" />
           <Route exact path="/" component={HomePage} />
           <Route path="/createpoll" component={CreatePoll} />
         </div>
--- src/CreatePoll.js
+++ src/CreatePoll.js
@@ -1,12 +1,86 @@
 import React, { Component } from "react";
+import firebase from './firebase';
 // import { Link } from "react-router-dom";
 
 class CreatePoll extends Component {
+  constructor() {
+    super();
+    this.state = {
+      // polls: [],
+      titleInput: "",
+      questionInput: "",
+      optionOneInput: "",
+      optionTwoInput: "",
+      errorMessage: ""
+    }
+  }
+
+  handleTitle = (event) => {
+    this.setState({
+      titleInput: event.target.value
+    })
+  }
+  handleQuestion = (event) => {
+    this.setState({
+      questionInput: event.target.value
+    })
+  }
+  handleOptionOne = (event) => {
+    this.setState({
+      optionOneInput: event.target.value
+    })
+  }
+  handleOptionTwo = (event) => {
+    this.setState({
+      optionTwoInput: event.target.value
+    })
+  }
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    const dbRef = firebase.database().ref();
+    if (this.state.questionInput !== "" && this.state.titleInput !=="" && this.state.OptionOneInput !=="" && this.state.optionTwoInput !=="" ) {
+      const pollObject = {
+        titleInput: this.state.titleInput,
+        questionInput: this.state.questionInput,
+        optionOneInput: this.state.optionOneInput,
+        optionTwoInput: this.state.optionTwoInput
+      }
+      dbRef.push(pollObject);
+
+      // reset error handle and clear input text
+      this.setState({
+        errorMessage: '',
+        titleInput: "",
+        questionInput: "",
+        optionOneInput: "",
+        optionTwoInput: "",
+      })
+    } else {
+      // error handling
+      this.setState ({
+        errorMessage: "Please fill in all inputs before submitting",
+      })
+    }
+  }
+
   render() {
     return (
       <main>
         <section>
-          <h1>Poll</h1>
+          <h2>Create your Poll</h2>
+          <form action="/" onSubmit={this.handleSubmit}>
+            <label htmlFor="titleInput">title</label>
+            <input type="text" id="titleInput"  value={this.state.titleInput} onChange={this.handleTitle}/>
+            <label htmlFor="questionInput">question</label>
+            <input type="text" id="questionInput"  value={this.state.questionInput} onChange={this.handleQuestion}/>
+            <label htmlFor="optionOne">option one</label>
+            <input type="text" id="optionOne" value={this.state.optionOneInput} onChange={this.handleOptionOne}/>
+            <label htmlFor="optionTwo">option two</label>
+            <input type="text" id="optionTwo" value={this.state.optionTwoInput} onChange={this.handleOptionTwo}/>
+            <button type="submit">Create Poll</button>
+          </form>
+          <p>{this.state.errorMessage}</p>
         </section>
       </main>
     );
