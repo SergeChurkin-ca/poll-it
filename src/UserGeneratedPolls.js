a99897045b190b475e2e2d268f8f198b94f53456
ajtoure <assiyahjamilla@gmail.com>
2020-08-16 18:48:36 -0400

    changes made to user generated polls, id variable, constructor

--- src/UserGeneratedPolls.js
+++ src/UserGeneratedPolls.js
@@ -5,75 +5,53 @@ import { Link } from "react-router-dom";
 class UserGeneratedPolls extends Component {
   constructor() {
     super();
-    this.database = firebase.database().ref();
     this.state = {
-      polls: [],
+      poll: {},
     };
   }
 
-  // **************************
   componentDidMount() {
-    const dbRef = firebase
+    const key = this.props.match.params.actualId;
+    firebase
       .database()
-      .ref()
+      .ref(key)
       .on("value", (snapshot) => {
         console.log(snapshot.val());
+        this.setState({
+          poll: snapshot.val(),
+        })
       });
+    
   }
 
-  // dbRef.on("value", (snapshot) => {
-  //   // checking changes in db
-
-  //   const data = snapshot.val();
-  //   const newToursAarray = [];
-
-  //   for (let inventoryName in data) {
-  //     const toursObject = {
-  //       id: inventoryName,
-  //       optionOne: data[inventoryName].optionOneInput,
-  //       optionTwo: data[inventoryName].optionTwoInput,
-  //       questionInput: data[inventoryName].questionInput,
-  //       titleInput: data[inventoryName].titleInput,
-  //     };
-  //     newToursAarray.push(toursObject);
-  //   }
-
-  //   this.setState({
-  //     polls: newToursAarray,
-  //   });
-  // });
-  // }
-
   render() {
-    // console.log(this.props.match.params.actualId);
+    console.log(this.props.match.params.actualId);
     return (
-      <div className="tourlist">
+      <form>
         <h1> User Generated Polls </h1>
-        {this.state.polls.map((pollsObject) => {
-          return (
-            <ul className="inventoryItem" key={pollsObject.id}>
-              <h2> {pollsObject.titleInput} </h2>
-              <h3> Please chose on option {pollsObject.questionInput}: </h3>
-              <li>
-                <input
-                  type="radio"
-                  name="one"
-                  value={pollsObject.optionOne}
-                ></input>
-                {pollsObject.optionOne}
-              </li>
-              <li>
-                <input
-                  type="radio"
-                  name="two"
-                  value={pollsObject.optionTwo}
-                ></input>
-                {pollsObject.optionTwo}
-              </li>
-            </ul>
-          );
-        })}
-      </div>
+              <h2>{this.state.poll.titleInput}</h2>
+              <h3>{this.state.poll.questionInput}:</h3>
+                <label htmlFor="one">
+                  {this.state.poll.optionOneInput}
+                  <input
+                    type="radio"
+                    name="options"
+                    id="one"
+                    value={this.state.poll.optionOne}
+                  ></input>
+                </label>
+                {this.state.poll.optionOne}
+                <label htmlFor="two">
+                  {this.state.poll.optionTwoInput}
+                  <input
+                    type="radio"
+                    name="options"
+                    id="two"
+                    value={this.state.poll.optionTwo}
+                  ></input>
+                </label>
+                {this.state.poll.optionTwo}
+      </form>
     );
   }
 }
