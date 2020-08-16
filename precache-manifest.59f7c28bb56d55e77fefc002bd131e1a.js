9a6a7648481ba8cc727b664d66631ac530cf3557
lizzSoup <elizabeth.ann.soup@gmail.com>
2020-08-16 17:36:02 -0400

    Updates

---
+++ precache-manifest.59f7c28bb56d55e77fefc002bd131e1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b7ce13d8790cbbadfc38cee88c74633",
+    "url": "/anonymousVotingBooth/index.html"
+  },
+  {
+    "revision": "15dc4922e7ea9d6eef78",
+    "url": "/anonymousVotingBooth/static/css/main.009afb9e.chunk.css"
+  },
+  {
+    "revision": "0ec37abddcdb3f4222dc",
+    "url": "/anonymousVotingBooth/static/js/2.5a5f0cb3.chunk.js"
+  },
+  {
+    "revision": "4224b32d50aca7d0b4f248bf0384ed4f",
+    "url": "/anonymousVotingBooth/static/js/2.5a5f0cb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15dc4922e7ea9d6eef78",
+    "url": "/anonymousVotingBooth/static/js/main.42d3d8bf.chunk.js"
+  },
+  {
+    "revision": "273d1a624861adc81dd1",
+    "url": "/anonymousVotingBooth/static/js/runtime-main.066e2072.js"
+  }
+]);
