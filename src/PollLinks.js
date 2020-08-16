a31bf316dd10060f8fbde146ca2ca5873c4f6737
lizzSoup <elizabeth.ann.soup@gmail.com>
2020-08-16 10:26:53 -0400

    Progress on routing/key/firebase problem

--- src/PollLinks.js
+++ src/PollLinks.js
@@ -1,12 +1,33 @@
 import React, { Component } from "react";
+import firebase from "./firebase";
 
 class PollLinks extends Component {
+  constructor() {
+    super();
+    this.state = {
+      poll: {},
+    };
+  }
+  componentDidMount() {
+    firebase
+      .database()
+      .ref("-MErTQrvxBAorWYrPPTY")
+      .on("value", (snapshot) => {
+        this.setState({
+          poll: snapshot.val(),
+        });
+      });
+  }
+
   render() {
+    console.log(this.state.poll.titleInput);
     return (
       <div>
         <p>The User ID is: {this.props.match.params.pollId}</p>
-        <h1>Poll Links Worked</h1>
-        <p>THIS IS FINALLY WORKING!! AHHHHH</p>
+        <h1>The title is: {this.state.poll.titleInput}</h1>
+        <p>The question is: {this.state.poll.questionInput}</p>
+        <p>The first option is: {this.state.poll.optionOneInput}</p>
+        <p>The second option is: {this.state.poll.optionTwoInput}</p>
       </div>
     );
   }
