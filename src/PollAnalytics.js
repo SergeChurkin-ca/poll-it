3ab54318940ce3389b61e871510ab412949315d3
lizzSoup <elizabeth.ann.soup@gmail.com>
2020-08-18 18:26:18 -0400

    Udating styles for analytics page

--- src/PollAnalytics.js
+++ src/PollAnalytics.js
@@ -2,6 +2,7 @@
 import React, { Component } from "react";
 import firebase from "./firebase";
 import { Link } from "react-router-dom";
+import "./analytics.css";
 
 // Component ----- +
 class PollAnalytics extends Component {
@@ -30,17 +31,37 @@ class PollAnalytics extends Component {
     const key = this.props.match.params.pollKey;
     const poll = this.state.poll;
     return (
-      <div>
-        <h1> The title is: {poll.title} </h1>
-        <p> The question is: {poll.question} </p>
-        <p> The first option is: {poll.optionA} </p>
-        <p>{poll.optionACount}</p>
-        <p>{poll.optionBCount}</p>
-        <p>{poll.optionACount + poll.optionBCount}</p>
-        <p> The second option is: {poll.optionB} </p>
-        <p>Need more votes? Share your poll with the link below!</p>
-        <Link to={`/polls/${key}/view`}>Share your poll!</Link>
-      </div>
+      <main className="pageContainer analytics">
+        <h2>Your Poll Analytics!</h2>
+        <p>
+          As poll creater, you can spy on the anonymous votes before anyone
+          else. Whenever you are ready to make your research public, reveal the
+          results with your friends and family with the click of a button!
+        </p>
+        <section className="analyticsInfo">
+          <h2>{poll.title}</h2>
+          <p className="pollQuestion">{poll.question}</p>
+          <p> Option A: {poll.optionA}</p>
+          <p> Option B: {poll.optionA}</p>
+          <div className="pollCounters">
+            <div className="count countA">
+              <p>A votes:</p>
+              <p>{poll.optionACount}</p>
+            </div>
+            <div className="count countB">
+              <p>B votes: </p>
+              <p>{poll.optionBCount}</p>
+            </div>
+            <div className="count countTotal">
+              <p>Total:</p>
+              <p>{poll.optionACount + poll.optionBCount}</p>
+            </div>
+          </div>
+          <p> The second option is: {poll.optionB} </p>
+          <p>Need votes? Share your poll with the link below!</p>
+          <Link to={`/polls/${key}/view`}>Share your poll!</Link>
+        </section>
+      </main>
     );
   }
 }
